ff9caeb8e6c496b68b4a3f0483222db280db3041
Daniel Riroche Pereira Ogasawara <danielhiro13@gmail.com>
2023-07-08 17:03:09 -0300

    refactor: Improving functions and errors handling

--- src/slashCommands/moderation.ts
+++ src/slashCommands/moderation.ts
@@ -4,6 +4,8 @@ import {
   TextChannel,
 } from 'discord.js';
 import { SlashCommand } from '../types';
+import { genericErrorMessage } from '../utils/errors';
+import { setTimeout as wait } from 'node:timers/promises';
 
 const command: SlashCommand = {
   command: new SlashCommandBuilder()
@@ -12,51 +14,65 @@ const command: SlashCommand = {
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .addSubcommand((subcommand) =>
       subcommand
-        .setName('apagar')
+        .setName('limpar')
         .setDescription('Apaga as mensagens do chat.')
         .addNumberOption((option) => {
           return option
             .setName('quantidade')
-            .setRequired(true)
+            .setDescription('Quantidade de mensagens para serem apagadas.')
             .setMinValue(1)
-            .setDescription('Quantidade de mensagens para serem apagadas.');
+            .setRequired(true);
         })
     ),
   execute: async (interaction) => {
     const subcommandName = interaction.options.getSubcommand(true);
     switch (subcommandName) {
-      case 'apagar':
-        try {
-          const limit = interaction.options.getNumber('quantidade', true);
-          const channel = interaction.channel;
-          const messagesInChannel = await interaction.channel?.messages.fetch({
-            limit: limit,
-          });
+      case 'limpar':
+        const limit = interaction.options.getNumber('quantidade', true);
+        const channel = interaction.channel;
+        const messagesInChannel = await interaction.channel?.messages.fetch({
+          limit: limit,
+        });
 
-          if (messagesInChannel?.size === 0)
-            return interaction.reply({
-              content: 'Não há mensagens para apagar.',
-              ephemeral: true,
-            });
-
-          if (channel instanceof TextChannel) {
-            channel.bulkDelete(limit).finally(() =>
-              interaction.reply({
-                content: 'Mensagem(ns) apagadas!',
-                ephemeral: true,
-              })
-            );
-          }
-        } catch (error) {
-          interaction.reply({
-            content: 'Algo deu errado...',
+        if (messagesInChannel?.size === 0) {
+          return interaction.reply({
+            content: 'Não há mensagens para apagar.',
             ephemeral: true,
           });
+        }
+
+        if (channel instanceof TextChannel) {
+          return channel
+            .bulkDelete(limit, true)
+            .then(async (messages) => {
+              if (messages.size === 0) {
+                interaction.reply({
+                  content:
+                    'Mensagens com mais de 14 dias não puderam ser apagadas.',
+                  ephemeral: true,
+                });
+                await wait(10000);
+                interaction.deleteReply();
+              } else {
+                interaction.reply({
+                  content: `${messages.size} mensagem(ns) apagada(s) com sucesso!`,
+                  ephemeral: true,
+                });
+                await wait(10000);
+                interaction.deleteReply();
+              }
+            })
+            .catch((reason) => {
+              interaction.reply({
+                content: genericErrorMessage,
+              });
+              console.error(reason);
+            });
         }
         break;
 
       default:
-        interaction.reply({ content: 'Algo deu errado...' });
+        interaction.reply({ content: genericErrorMessage });
         break;
     }
   },
